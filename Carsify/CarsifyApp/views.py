01320ab8871aaed67cafeac81278c51d9df82974
Anshuman Prajapati <44315336+anshumanprajapatiap@users.noreply.github.com>
2021-09-17 11:04:40 +0530

    Image

--- Carsify/CarsifyApp/views.py
+++ Carsify/CarsifyApp/views.py
@@ -353,6 +353,57 @@ def resize_img(img_path, img_name):
 
     return 'new'+img_name
 
+def resize_img_show(img_path, img_name):
+    pic=cv2.imread(img_path)
+
+    # canvas_size=(333,500)
+    if 333/500 < pic.shape[0]/pic.shape[1]:
+        x=333
+        temp=(333-pic.shape[0])/pic.shape[0]
+        y=pic.shape[1]+(temp*pic.shape[1])
+    else:
+        y=500
+        temp=(500-pic.shape[1])/pic.shape[1]
+        x=pic.shape[0]+(temp*pic.shape[0])
+    
+    width = int(y)
+    height = int(x)
+    dim = (width, height)
+    resized = cv2.resize(pic, dim, interpolation = cv2.INTER_AREA)
+        
+    img_pic = np.zeros([333,500,3],dtype=np.uint8)
+    img_pic[:] = 255
+    pad_x=0
+    pad_y=0
+    if width<500:
+        pad_x=(500-width)//2
+    if height<333:
+        pad_y=(333-height)//2
+    img_pic[pad_y:pad_y+height,pad_x:pad_x+width]=resized
+    
+    font = cv2.FONT_HERSHEY_SIMPLEX
+  
+    # org
+    org = (100, 200)
+    
+    # fontScale
+    fontScale = 1
+    
+    # Blue color in BGR
+    color = (180,201,94)
+    
+    # Line thickness of 2 px
+    thickness = 2
+    
+    # Using cv2.putText() method
+    img_pic = cv2.putText(img_pic, 'Copyright Carsify.in', org, font, fontScale, color, thickness, cv2.LINE_AA)
+
+   
+    
+    cv2.imwrite('media/show'+img_name, img_pic)
+
+    return 'show'+img_name
+
 
 
 @login_required(login_url='CarsifyApp:LoginSignup')
@@ -408,7 +459,7 @@ def Addcar(request):
         imm = cid.Showimage
         img_path = imm.path
         img_name = str(imm)
-        new_img = resize_img(img_path, img_name)
+        new_img = resize_img_show(img_path, img_name)
         cid.Showimage = new_img
         cid.save()
         os.remove(img_path)
